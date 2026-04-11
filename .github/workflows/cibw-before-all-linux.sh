9e7192f41b14468f0dd1151a86803f13390fd049
William Stein <wstein@sagemath.com>
2026-04-11 09:08:43 -0700

    packaging: ignore wheel constraints when bootstrapping python

--- .github/workflows/cibw-before-all-linux.sh
+++ .github/workflows/cibw-before-all-linux.sh
@@ -7,8 +7,8 @@ SPKGS="${SPKGS:-_bootstrap _prereq}"
 TARGETS_PRE="${TARGETS_PRE:-gmp mpfr mpc bliss coxeter3 mcqd meataxe sirocco tdlib}"
 SAGE_PYTHON="${SAGE_PYTHON:-/opt/python/cp312-cp312/bin/python3}"
 
-"${SAGE_PYTHON}" -m ensurepip --upgrade || true
-"${SAGE_PYTHON}" -m pip install --upgrade pip setuptools wheel
+env -u PIP_CONSTRAINT "${SAGE_PYTHON}" -m ensurepip --upgrade || true
+env -u PIP_CONSTRAINT "${SAGE_PYTHON}" -m pip install --upgrade pip setuptools wheel
 
 echo "Installing bootstrap prerequisites inside cibuildwheel container"
 (
